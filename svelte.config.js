a2beaeadad4d1bce221ccccbb9829d84540b6bc5
Pedro <rm88246@fiap.com.br>
2024-09-27 17:48:22 -0300

    style: formatação de arquivos

--- svelte.config.js
+++ svelte.config.js
@@ -6,20 +6,20 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			$components: './src/components',
-			$lib: './src/lib',
-			$stores: './src/stores',
-			$utils: './src/utils',
+			'$components': './src/components',
+			'$lib': './src/lib',
+			'$stores': './src/stores',
+			'$utils': './src/utils',
 			'$app.postcss': './src/app.postcss',
 		},
-	}
+	},
 };
 
 export default config;
