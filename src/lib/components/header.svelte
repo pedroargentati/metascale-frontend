abae35bf16fd0a9466ea9b0bb683703302de8b00
Lucas Alexander <lucas.claudino@populisrh.com>
2024-09-28 01:40:37 -0300

    feat: melhorias no layout, estilização e transição dos itens

--- src/lib/components/header.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let title: string = '';
-
-</script>
-
-<header class="navbar bg-neutral">
-	<h1 class="font-semibold text-lg font-semibold">{title}</h1>
-</header>
--- src/lib/components/menu-item.svelte
+++ src/lib/components/menu-item.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	
+
 	export let icon: string;
 	export let link: string;
+	export let label: string;
 </script>
 
 <li>
-	<a class="btn p-0 bg-transparent" href={link}>
-		<Icon
-			icon={icon}
-			class="size-12 text-base-content hover:text-primary"
-		/>
+	<a class="flex gap-2 bg-transparent hover:scale-110 transition-all" href={link}>
+		<Icon icon={icon} class="size-6 text-primary" />
+		<span class="text-primary">{label}</span>
 	</a>
 </li>
