e689f4c72a0aa33c8c4cbe2ec4e7928ee209195b
Pedro <rm88246@fiap.com.br>
2024-09-29 16:54:27 -0300

    feat: getAll Canonico created

--- src/server/services/canonical.ts
+++ src/server/services/canonical.ts
@@ -9,6 +9,16 @@ export class CanonicoService {
 	private static BASE_URL: string = 'http://localhost:8080/canonico';
 
 	/**
+	 * Busca todos os Canonicos.
+	 *
+	 * @returns {Promise<ICanonico[]>} Uma promessa que resolve com um array de objetos do tipo ICanonico.
+	 * @throws {Error} Se houver um erro ao realizar a requisição ou se a resposta não for bem-sucedida.
+	 */
+	public static async getAllCanonicos(): Promise<ICanonico[]> {
+		return await CoreApi.get<ICanonico[]>(this.BASE_URL);
+	}
+
+	/**
 	 * Busca todos os Canonicos com o status especificado.
 	 *
 	 * @param {Status} status - Status do Canonico a ser buscado (por exemplo, ACTIVE ou INACTIVE).
